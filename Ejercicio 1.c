152c7c622f7a50a4f33f67e7b37afba3ddc1bc9e
gonzalo <you@example.com>
2021-06-07 14:00:14 -0300

    Ejercicios introduccion a C

---
+++ Ejercicio 1.c
@@ -0,0 +1,8 @@
+// hello.c
+#include <stdio.h>
+
+int main(void) {
+printf("Hello world\n");printf("Hello world\n");printf("Hello world\n");printf("Hello world\n");printf("Hello world\n");
+printf("Hello world\n");printf("Hello world\n");printf("Hello world\n");printf("Hello world\n");printf("Hello world\n");
+  return 0;
+}
